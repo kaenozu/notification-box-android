bedf31cd313a282322c45646a9437524bc6304c2
kaenozu <106388957+kaenozu@users.noreply.github.com>
2026-07-16 22:45:09 +0900

    refactor: narrow notification permission platform

--- app/src/main/java/com/notificationbox/app/permission/NotificationPermissionPlatform.kt
+++ app/src/main/java/com/notificationbox/app/permission/NotificationPermissionPlatform.kt
@@ -1,9 +1,6 @@
 package com.notificationbox.app.permission
 
 interface NotificationPermissionPlatform {
-    val sdkInt: Int
     val packageName: String
-    fun hasPostNotificationsPermission(): Boolean
-    fun areNotificationsEnabled(): Boolean
     fun enabledListenerPackages(): Set<String>
 }
